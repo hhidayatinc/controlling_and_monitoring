67cc098874e0a061f62e5b6007470acd595fcea9
Hidayati Nur Chasanah <chasanah55@gmail.com>
2022-05-15 15:49:42 +0700

    new code

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <ArduinoJson.h>
 
-const char *ssid = "Rizqi";
-const char *password = "rizqioke";
+const char *ssid = ".";
+const char *password = "12345678";
 const char *mqtt_server = "test.mosquitto.org";
 LiquidCrystal_I2C lcd(0x27, 16, 2); // lcd sda 21, scl 22
 #define ONE_WIRE_BUS 5              // Digitalpin where Temp sensor is connected
@@ -28,17 +28,19 @@ DallasTemperature sensors(&oneWire);
 const int trigPin = 4;
 const int echoPin = 18;
 long duration;
-float distanceCm = 0;
-float tdsValue = 0;
-float suhu = 0;
 float calibration_value = 21.34 - 0.7;
 int phval = 0;
 unsigned long int avgval;
 int buffer_arr[10], temp;
 float ph_act;
-
-// add some variables for pump (water, nutrition a, b) boolean
-// add variable for time
+int water = 0;
+int tdsValue = 0;
+int suhu = 0;
+uint8_t relay1 = 27; // nutrisiA
+uint8_t relay2 = 26; // nutrisiB
+uint8_t relay3 = 14; // air
+bool pump_water = false;
+bool pump_nutrisi = false;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -47,160 +49,54 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-// In the callback() function, the ESP32 receives the MQTT messages of the subscribed topics.
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // // Switch on the LED if an 1 was received as first character
-  // if ((char)payload[0] == '1')
-  // {
-  //   digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
-  //   // but actually the LED is on; this is because
-  //   // it is active low on the ESP-01)
-  // }
-  // if ((char)payload[0] == '0')
-  // {
-  //   digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
-  // }
-}
-
-void save_data(){
-  // save data to flash memory
-  Serial.println("Saving data to flash memory");
-  EEPROM.write(0, 1);
-  EEPROM.write(1, suhu);
-  EEPROM.write(2, tdsValue);
-  EEPROM.write(3, ph_act);
-  EEPROM.write(4, distanceCm);
-  EEPROM.commit();
-  Serial.println("Data saved");
-  delay(10000);
-}
-
-void reconnect()
-{
-  // Loop until we're reconnected
-  while (!client.connected())
-  {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "HidroponikClient-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str()))
-    {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("hidroponik/esp32/ph", String(ph_act).c_str());
-      client.publish("hidroponik/esp32/suhu", String(suhu).c_str());
-      client.publish("hidroponik/esp32/tds", String(tdsValue).c_str());
-      client.publish("hidroponik/esp32/distance", String(distanceCm).c_str());
-      // ... and resubscribe
-      client.subscribe("hidroponik/esp32/ph");
-      client.subscribe("hidroponik/esp32/suhu");
-      client.subscribe("hidroponik/esp32/tds");
-      client.subscribe("hidroponik/esp32/distance");
-    }
-    else // if !client.connect() call func save data to local direcotry(flash memory)
-    {
-      Serial.print("Saving data sensor to flash memory -----");
-      save_data();
-      Serial.print("Connection server failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" Try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-void setup_wifi()
-{
-  // wifi
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting...");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("Connected: ");
-  Serial.println(WiFi.localIP());
-}
-
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, HIGH);
+  digitalWrite(relay3, HIGH);
+
   sensors.begin();
   gravityTds.setPin(TdsSensorPin);
-  gravityTds.setAref(3.3);      // reference voltage on ADC, default 5.0V on Arduino UNO
-  gravityTds.setAdcRange(4096); // 1024 for 10bit ADC;4096 for 12bit ADC
-  gravityTds.begin();           // initialization
+  gravityTds.setAref(3.3);
+  gravityTds.setAdcRange(4096);
+  gravityTds.begin();
   Wire.begin();
-
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
 
   lcd.init();
   lcd.backlight();
 }
 
-void read_sensor()
-{
-  // start nutrisi
-  sensors.requestTemperatures();
-  gravityTds.setTemperature(sensors.getTempCByIndex(0)); // grab the temperature from sensor and execute temperature compensation
-  gravityTds.update();                                   // calculation done here from gravity library
-  tdsValue = gravityTds.getTdsValue();                   // then get the TDS value
-  if (client.connected())
-  {
-    char nutString[8];
-    dtostrf(tdsValue, 1, 2, nutString);
-    client.publish("hidroponik/esp32/tds", nutString);
-  }
-  Serial.printf("Nutrisi: %f\n", tdsValue);
-  lcd.setCursor(0, 0);
-  lcd.printf("N: %f ppm", tdsValue);
+void save_data(){
+  EEPROM.write(0, phval);
+  EEPROM.write(1, tdsValue);
+  EEPROM.write(2, suhu);
+  EEPROM.write(3, water);
+  EEPROM.write(4, pump_water);
+  EEPROM.write(5, pump_nutrisi);
+  EEPROM.commit();
 
-  delay(2500);
-  lcd.clear();
-  // end nutrisi
+  Serial.println("Data saved");
+  //delay(1000);
+}
 
-  // start suhu
+void read_sensor(){
+  //suhu
   sensors.requestTemperatures();
   suhu = sensors.getTempCByIndex(0);
-  if (client.connected())
-  {
-    char tempString[8];
-    dtostrf(suhu, 1, 2, tempString);
-    client.publish("hidroponik/esp32/suhu", tempString);
-  }
+  Serial.printf("suhu: %d\n", suhu);
+  lcd.setCursor(0, 0);
+  lcd.printf("Suhu: %d", suhu);
 
-  Serial.printf("Suhu: %f\n", suhu);
-
-  lcd.setCursor(0, 1);
-  lcd.printf("T: %f C", suhu);
-
-  delay(2500);
-  lcd.clear();
-  // end suhu
-
-  // start pH
-  for (int i = 0; i < 10; i++)
-  {
+  //ph
+  for (int i = 0; i < 9; i++){
     buffer_arr[i] = analogRead(32);
     delay(30);
   }
@@ -218,71 +114,230 @@ void read_sensor()
   }
   avgval = 0;
   for (int i = 2; i < 8; i++)
-    avgval += buffer_arr[i];
-  float volt = (float)avgval * 3.3 / 4095 / 6;
-  ph_act = -5.70 * volt + calibration_value;
-  if (client.connected())
   {
-    char pHString[8];
-    dtostrf(ph_act, 1, 2, pHString);
-    client.publish("hidroponik/esp32/ph", pHString);
+    avgval += buffer_arr[i];
+    float volt = (float)avgval * 3.3 / 4096 / 6;
+    ph_act = -5.70 * volt + calibration_value;
   }
+  phval = (int)ph_act;
+  Serial.printf("ph: %d\n", phval);
+  lcd.setCursor(0, 1);
+  lcd.printf("Ph: %d", phval);
 
-  Serial.printf("pH: %f\n", ph_act);
-  lcd.setCursor(3, 0);
-  lcd.printf("pH: %f ", ph_act);
+  //tds
+  sensors.requestTemperatures();
+  gravityTds.setTemperature(sensors.getTempCByIndex(0));
+  gravityTds.update();
+  tdsValue = gravityTds.getTdsValue();
+  Serial.printf("tds: %d\n", tdsValue);
+  lcd.setCursor(2, 0);
+  lcd.printf("TDS: %d", tdsValue);
 
-  delay(2500);
-  lcd.clear();
-  // end pH
-
-  // start distance
-  // Clears the trigPin
+  //water
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
+  water = duration * SOUND_SPEED / 2;
+  Serial.printf("water: %d\n", water);
+  lcd.setCursor(2, 1);
+  lcd.printf("Water: %d", water);
 
-  // Calculate the distance
-  distanceCm = duration * SOUND_SPEED / 2;
-
-  // Prints the distance in the Serial Monitor
-  if (client.connected())
-  {
-    char distanceString[8];
-    dtostrf(distanceCm, 1, 2, distanceString);
-    client.publish("hidroponik/esp32/distance", distanceString);
-  }
-
-  Serial.printf("Distance (cm): %f\n", distanceCm);
-
-  lcd.setCursor(3, 1);
-  lcd.printf("A: %f ", distanceCm);
   delay(2500);
-  lcd.clear();
-  // end distance
 }
 
-void loop()
-{
-  if (!client.connected())
+void setup_wifi(){
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED){
+    //Serial.print(".");
+    Serial.println("Failed connect internet!");
+    Serial.println("Start to read sensor ....");
+    read_sensor();
+    Serial.println("Checking water & nutrient ....");
+    water_condition();
+    Serial.println("Saving all data ....");
+    save_data();
+    //read_sensor();
+    
+    delay(1000);
+  }
+  Serial.println();
+  Serial.print("Connected: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void water_condition(){
+  if (water >= 60){
+    digitalWrite(relay3, LOW);
+    pump_water = true;
+    Serial.printf("Pump water: %d\n", pump_water);
+    lcd.setCursor(3, 0);
+    lcd.print("Air: ON");
+  }
+  else{
+    digitalWrite(relay3, HIGH);
+    pump_water = false;
+    Serial.printf("Pump water: %d\n", pump_water);
+    lcd.setCursor(3, 1);
+    lcd.print("Air: OFF");
+    nutrient_condition();
+  }
+}
+
+void nutrient_condition(){
+  if (tdsValue <= 140){
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+    digitalWrite(relay3, HIGH);
+    pump_nutrisi = true;
+    Serial.printf("Pump nutrisi: %d\n", pump_nutrisi);
+    lcd.setCursor(4, 0);
+    lcd.print("Nutrient: ON");
+  }
+  else{
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    pump_nutrisi = false;
+    Serial.printf("Pump nutrisi: %d\n", pump_nutrisi);
+    lcd.setCursor(4, 1);
+    lcd.print("Nutrient: OFF");
+  }
+}
+
+void publish_sensor(){
+  client.publish("/sensor/ph", String(phval).c_str());
+  client.publish("/sensor/tds", String(tdsValue).c_str());
+  client.publish("/sensor/suhu", String(suhu).c_str());
+  client.publish("/sensor/water", String(water).c_str());
+}
+
+void subscribe_control(){
+  client.subscribe("/control/water");
+  client.subscribe("/control/tds");
+} 
+
+void publish_control(){
+  client.publish("/control/water", String(pump_water).c_str());
+  client.publish("/control/tds", String(pump_nutrisi).c_str());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  //setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("HidroponikClient-")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      Serial.println("Start to read sensor ....");
+      read_sensor();
+      Serial.println("Checking water & nutrient ....");
+      water_condition();
+      Serial.println("Saving all data ....");
+      save_data();
+      //Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//void save_data(){
+//  File dataFile = SPIFFS.open("/data.txt", "w");
+//  if (!dataFile) {
+//    Serial.println("Failed to open data file for writing");
+//    return;
+//  }
+//  dataFile.print(phval);
+//  dataFile.print("\n");
+//  dataFile.print(tdsValue);
+//  dataFile.print("\n");
+//  dataFile.print(suhu);
+//  dataFile.print("\n");
+//  dataFile.print(water);
+//  dataFile.print("\n");
+//  dataFile.print(pump_water);
+//  dataFile.print("\n");
+//  dataFile.print(pump_nutrisi);
+//  dataFile.print("\n");
+//  dataFile.close();
+//
+//  Serial.println("Data saved");
+//  delay(1000);
+//}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++)
   {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (topic == "/control/water"){
+    if (messageTemp == "true"){
+      pump_water = true;
+      Serial.printf("Pump water: %d\n", pump_water);
+      lcd.setCursor(3, 0);
+      lcd.print("Air: ON");
+    }
+    else if (messageTemp == "false"){
+      pump_water = false;
+      Serial.printf("Pump water: %d\n", pump_water);
+      lcd.setCursor(3, 1);
+      lcd.print("Air: OFF");
+    }
+  }
+  else if (topic == "/control/tds"){
+    if (messageTemp == "true"){
+      pump_nutrisi = true;
+      Serial.printf("Pump nutrisi: %d\n", pump_nutrisi);
+      lcd.setCursor(4, 0);
+      lcd.print("Nutrient: ON");
+    }
+    else if (messageTemp == "false"){
+      pump_nutrisi = false;
+      Serial.printf("Pump nutrisi: %d\n", pump_nutrisi);
+      lcd.setCursor(4, 1);
+      lcd.print("Nutrient: OFF");
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  read_sensor();
+  water_condition();
+  //nutrient_condition();
+  //save_data();
+  setup_wifi();
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
   long now = millis();
-  if (now - lastMsg > 5000)
-  {
+  if (now - lastMsg > 5000) {
     lastMsg = now;
-    // put your main code here, to run repeatedly:
-    // read sensor
-    read_sensor();
-  }
-}
 
-// create func to trun on pump and off pump
+    publish_sensor();
+    subscribe_control();
+    publish_control(); 
+  }
+
+  delay(2500);
+}
